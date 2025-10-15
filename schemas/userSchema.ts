fdd283e91f563e58aec0522f77394f19865d5802
amakarenko1203 <annamakarenko.rdh@gmail.com>
2025-10-14 19:46:06 -0500

    ran user test

--- schemas/userSchema.ts
+++ schemas/userSchema.ts
@@ -1,7 +1,33 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
-export const expectedCreateUserResponseSchema  = z.object({
-            code: z.literal(200),
-            type: z.literal("unknown"),
-            message: z.string()
+// User schema for Get User API response
+export const UserSchema = z.object({
+    id: z.number(),
+    username: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+    phone: z.string(),
+    userStatus: z.number()
 });
+
+// Create User Request Schema
+export const CreateUserRequestSchema = z.object({
+    id: z.number(),
+    username: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string(),
+    password: z.string(),
+    phone: z.string(),
+    userStatus: z.number()
+});
+
+// Create User Response Schema
+export const CreateUserResponseSchema = z.object({
+    code: z.literal(200),
+    type: z.literal("unknown"),
+    message: z.string()
+});
+
--- tests/userAPIs.spec.ts
+++ tests/userAPIs.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { z } from 'zod';
+import { UserSchema } from '../schemas/userSchema';
 
 
 test.describe('User API Tests', () => {
@@ -34,6 +35,26 @@ test.describe('User API Tests', () => {
         const actualResponseBody = await createUserResponse.json();
         expectedCreateUserResponseSchema.parse(actualResponseBody);
 
+    });
+
+    test('Get user by username', async ({ request }) => {
+        const username = "TestUserNameSalih123";
+        const getUserResponse = await request.get(`${process.env.BASE_URL}${process.env.API_VERSION}/user/${username}`);
+        expect(getUserResponse.status()).toBe(200);
+        
+        const expectedGetUserResponseSchemaZod = z.object({
+            "id": z.number(),
+            "username": z.literal(username),
+            "firstName": z.string(),
+            "lastName": z.string(),
+            "email": z.string(),
+            "password": z.string(),
+            "phone": z.string(),
+            "userStatus": z.number()
+        });
+        
+        const actualGetUserResponseBody = await getUserResponse.json();
+        expectedGetUserResponseSchemaZod.parse(actualGetUserResponseBody);
     });
 
 });
