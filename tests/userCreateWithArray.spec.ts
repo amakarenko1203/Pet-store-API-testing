7827e4cc176e1be0122664498d7db20c211d121c
amakarenko1203 <annamakarenko.rdh@gmail.com>
2025-10-17 18:47:28 -0500

    Update userCreateWithArray.spec.ts

--- tests/userCreateWithArray.spec.ts
+++ tests/userCreateWithArray.spec.ts
@@ -7,28 +7,20 @@ test.describe('User Create With Array Tests', () => {
     const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
 
     test("Creates list of users with given input array", async ({ request }) => {
-        const createUsersRequestBody = [
-            {
+        const createUsersRequestBody = [];
+
+        for (let i = 1; i <= 3; i++) {
+            createUsersRequestBody.push({
                 "id": faker.number.int({ min: 1, max: 100000 }),
-                "username": `User1-${faker.string.alphanumeric(6)}`,
+                "username": `User${i}-${faker.string.alphanumeric(6)}`,
                 "firstName": faker.person.firstName(),
                 "lastName": faker.person.lastName(),
                 "email": faker.internet.email(),
                 "password": faker.internet.password({ length: 10 }),
                 "phone": faker.phone.number(),
                 "userStatus": faker.number.int({ min: 0, max: 2 })
-            },
-            {
-                "id": faker.number.int({ min: 1, max: 100000 }),
-                "username": `User2-${faker.string.alphanumeric(6)}`,
-                "firstName": faker.person.firstName(),
-                "lastName": faker.person.lastName(),
-                "email": faker.internet.email(),
-                "password": faker.internet.password({ length: 10 }),
-                "phone": faker.phone.number(),
-                "userStatus": faker.number.int({ min: 0, max: 2 })
-            }
-        ];
+            });
+        }
 
         const responseCreateUsersWithArraySchemaZod = z.object({
             code: z.literal(200),
